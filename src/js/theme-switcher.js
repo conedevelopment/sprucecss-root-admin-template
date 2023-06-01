16244970adb8ea27ceb4f669435d56603598c5bc
Adam Laki <info@adamlaki.com>
2023-06-01 16:01:04 +0200

    WIP

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,12 +1,12 @@
 (() => {
   const themeSwitcher = document.querySelector('#theme-switcher');
-  const preferredTheme = localStorage.getItem('preferred-theme-demo') ?? 'system';
+  const preferredTheme = localStorage.getItem('preferred-theme') ?? 'system';
 
   themeSwitcher.addEventListener('input', (e) => {
     const theme = e.target.value;
     const systemMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-    localStorage.setItem('preferred-theme-demo', theme);
+    localStorage.setItem('preferred-theme', theme);
     document.documentElement.setAttribute('data-theme-mode', theme === 'system' ? systemMode : theme);
     themeSwitcher.setAttribute('data-theme-mode', theme);
     themeSwitcher.querySelector(`option[value="${theme}"]`).selected = 'selected';
