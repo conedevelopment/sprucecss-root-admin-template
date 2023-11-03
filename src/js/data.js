3284978bbf22084524c37ad70a62d76261389ade
Adam Laki <info@adamlaki.com>
2023-11-03 19:18:20 +0100

    WIP

--- src/js/data.js
+++ src/js/data.js
@@ -223,104 +223,124 @@ window.products = [
 
 window.orders = [
   {
-    orderId: 1,
-    name: "John Doe",
+    orderId: 20,
+    customer: "John Doe",
     total: 119.97,
-  },
-  {
-    orderId: 2,
-    name: "Jane Smith",
-    total: 97.95,
-  },
-  {
-    orderId: 3,
-    name: "David Johnson",
-    total: 254.94,
-  },
-  {
-    orderId: 4,
-    name: "Emily Davis",
-    total: 149.95,
-  },
-  {
-    orderId: 5,
-    name: "Michael Wilson",
-    total: 174.97,
-  },
-  {
-    orderId: 6,
-    name: "Sarah Thompson",
-    total: 189.98,
-  },
-  {
-    orderId: 7,
-    name: "Robert Martinez",
-    total: 109.97,
-  },
-  {
-    orderId: 8,
-    name: "Jennifer Anderson",
-    total: 79.98,
-  },
-  {
-    orderId: 9,
-    name: "Christopher Brown",
-    total: 169.97,
-  },
-  {
-    orderId: 10,
-    name: "Laura Davis",
-    total: 109.97,
-  },
-  {
-    orderId: 11,
-    name: "Matthew Taylor",
-    total: 144.97,
-  },
-  {
-    orderId: 12,
-    name: "Jessica Wilson",
-    total: 189.97,
-  },
-  {
-    orderId: 13,
-    name: "Daniel Johnson",
-    total: 199.97,
-  },
-  {
-    orderId: 14,
-    name: "Sophia Anderson",
-    total: 84.97,
-  },
-  {
-    orderId: 15,
-    name: "Alexander Smith",
-    total: 144.97,
-  },
-  {
-    orderId: 16,
-    name: "Olivia Thompson",
-    total: 89.99,
-  },
-  {
-    orderId: 17,
-    name: "William Martinez",
-    total: 119.98,
-  },
-  {
-    orderId: 18,
-    name: "Ava Davis",
-    total: 109.99,
+    status: "Pending",
   },
   {
     orderId: 19,
-    name: "James Wilson",
-    total: 99.99,
+    customer: "Jane Smith",
+    total: 97.95,
+    status: "Cancelled",
   },
   {
-    orderId: 20,
-    name: "Mia Thompson",
+    orderId: 18,
+    customer: "David Johnson",
+    total: 254.94,
+    status: "Paid",
+  },
+  {
+    orderId: 17,
+    customer: "Emily Davis",
+    total: 149.95,
+    status: "Paid",
+  },
+  {
+    orderId: 16,
+    customer: "Michael Wilson",
+    total: 174.97,
+    status: "Paid",
+  },
+  {
+    orderId: 15,
+    customer: "Sarah Thompson",
+    total: 189.98,
+    status: "Shipped",
+  },
+  {
+    orderId: 14,
+    customer: "Robert Martinez",
+    total: 109.97,
+    status: "Shipped",
+  },
+  {
+    orderId: 13,
+    customer: "Jennifer Anderson",
+    total: 79.98,
+    status: "Shipped",
+  },
+  {
+    orderId: 12,
+    customer: "Christopher Brown",
+    total: 169.97,
+    status: "Cancelled",
+  },
+  {
+    orderId: 11,
+    customer: "Laura Davis",
+    total: 109.97,
+    status: "Shipped",
+  },
+  {
+    orderId: 10,
+    customer: "Matthew Taylor",
+    total: 144.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 9,
+    customer: "Jessica Wilson",
+    total: 189.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 8,
+    customer: "Daniel Johnson",
+    total: 199.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 7,
+    customer: "Sophia Anderson",
+    total: 84.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 6,
+    customer: "Alexander Smith",
+    total: 144.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 5,
+    customer: "Olivia Thompson",
+    total: 89.99,
+    status: "Cancelled",
+  },
+  {
+    orderId: 4,
+    customer: "William Martinez",
+    total: 119.98,
+    status: "Delivered",
+  },
+  {
+    orderId: 3,
+    customer: "Ava Davis",
+    total: 109.99,
+    status: "Delivered",
+  },
+  {
+    orderId: 2,
+    customer: "James Wilson",
+    total: 99.99,
+    status: "Delivered",
+  },
+  {
+    orderId: 1,
+    customer: "Mia Thompson",
     total: 134.97,
+    status: "Delivered",
   }
 ];
 
