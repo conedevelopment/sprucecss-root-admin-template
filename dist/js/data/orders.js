5b0b575fafe4aa7d570d6c422b340a47bbd066b2
Adam Laki <info@adamlaki.com>
2023-11-04 10:12:56 +0100

    Refactoring

---
+++ dist/js/data/orders.js
@@ -0,0 +1,122 @@
+window.orders = [
+  {
+    orderId: 20,
+    customer: "John Doe",
+    total: 119.97,
+    status: "Pending",
+  },
+  {
+    orderId: 19,
+    customer: "Jane Smith",
+    total: 97.95,
+    status: "Cancelled",
+  },
+  {
+    orderId: 18,
+    customer: "David Johnson",
+    total: 254.94,
+    status: "Paid",
+  },
+  {
+    orderId: 17,
+    customer: "Emily Davis",
+    total: 149.95,
+    status: "Paid",
+  },
+  {
+    orderId: 16,
+    customer: "Michael Wilson",
+    total: 174.97,
+    status: "Paid",
+  },
+  {
+    orderId: 15,
+    customer: "Sarah Thompson",
+    total: 189.98,
+    status: "Shipped",
+  },
+  {
+    orderId: 14,
+    customer: "Robert Martinez",
+    total: 109.97,
+    status: "Shipped",
+  },
+  {
+    orderId: 13,
+    customer: "Jennifer Anderson",
+    total: 79.98,
+    status: "Shipped",
+  },
+  {
+    orderId: 12,
+    customer: "Christopher Brown",
+    total: 169.97,
+    status: "Cancelled",
+  },
+  {
+    orderId: 11,
+    customer: "Laura Davis",
+    total: 109.97,
+    status: "Shipped",
+  },
+  {
+    orderId: 10,
+    customer: "Matthew Taylor",
+    total: 144.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 9,
+    customer: "Jessica Wilson",
+    total: 189.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 8,
+    customer: "Daniel Johnson",
+    total: 199.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 7,
+    customer: "Sophia Anderson",
+    total: 84.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 6,
+    customer: "Alexander Smith",
+    total: 144.97,
+    status: "Delivered",
+  },
+  {
+    orderId: 5,
+    customer: "Olivia Thompson",
+    total: 89.99,
+    status: "Cancelled",
+  },
+  {
+    orderId: 4,
+    customer: "William Martinez",
+    total: 119.98,
+    status: "Delivered",
+  },
+  {
+    orderId: 3,
+    customer: "Ava Davis",
+    total: 109.99,
+    status: "Delivered",
+  },
+  {
+    orderId: 2,
+    customer: "James Wilson",
+    total: 99.99,
+    status: "Delivered",
+  },
+  {
+    orderId: 1,
+    customer: "Mia Thompson",
+    total: 134.97,
+    status: "Delivered",
+  }
+];
---
+++ dist/js/data/search.js
@@ -0,0 +1,32 @@
+window.search = [
+  {
+    "caption": "Shop / Products",
+    "url": "/products/",
+    "icon": "<path d='M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z'></path><line x1='3' y1='6' x2='21' y2='6'></line><path d='M16 10a4 4 0 0 1-8 0'></path>"
+  },
+  {
+    "caption": "Shop / Orders",
+    "url": "/orders/",
+    "icon": "<circle cx='9' cy='21' r='1'></circle><circle cx='20' cy='21' r='1'></circle><path d='M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6'></path>"
+  },
+  {
+    "caption": "Shop / Customers",
+    "url": "/customers/",
+    "icon": "<path d='M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2'></path><circle cx='9' cy='7' r='4'></circle><path d='M23 21v-2a4 4 0 0 0-3-3.87'></path><path d='M16 3.13a4 4 0 0 1 0 7.75'></path>"
+  },
+  {
+    "caption": "Shop / Categories",
+    "url": "/categories/",
+    "icon": "<polyline points='21 8 21 21 3 21 3 8'></polyline><rect x='1' y='3' width='22' height='5'></rect><line x1='10' y1='12' x2='14' y2='12'></line>"
+  },
+  {
+    "caption": "Account",
+    "url": "/account/",
+    "icon": null
+  },
+  {
+    "caption": "Settings",
+    "url": "/settings/",
+    "icon": null
+  }
+];
