5605c0ddad6a2b246fcee65d0e8c3e6ad8957414
Adam Laki <info@adamlaki.com>
2023-11-04 11:01:37 +0100

    Customers list

---
+++ src/js/data/customers.js
@@ -0,0 +1,142 @@
+window.customers = [
+  {
+    id: "1",
+    firstName: "John",
+    lastName: "Doe",
+    email: "johndoe@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "2",
+    firstName: "Jane",
+    lastName: "Smith",
+    email: "janesmith@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "3",
+    firstName: "David",
+    lastName: "Johnson",
+    email: "davidjohnson@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "4",
+    firstName: "Emily",
+    lastName: "Brown",
+    email: "emilybrown@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "5",
+    firstName: "Michael",
+    lastName: "Wilson",
+    email: "michaelwilson@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "6",
+    firstName: "Olivia",
+    lastName: "Taylor",
+    email: "oliviataylor@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "7",
+    firstName: "James",
+    lastName: "Miller",
+    email: "jamesmiller@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "8",
+    firstName: "Sophia",
+    lastName: "Anderson",
+    email: "sophiaanderson@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "9",
+    firstName: "Daniel",
+    lastName: "Thomas",
+    email: "danielthomas@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "10",
+    firstName: "Ava",
+    lastName: "Wilson",
+    email: "avawilson@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "11",
+    firstName: "Matthew",
+    lastName: "Lee",
+    email: "matthewlee@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "12",
+    firstName: "Isabella",
+    lastName: "Harris",
+    email: "isabellaharris@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "13",
+    firstName: "Ethan",
+    lastName: "Roberts",
+    email: "ethanroberts@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "14",
+    firstName: "Mia",
+    lastName: "Clark",
+    email: "miaclark@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "15",
+    firstName: "Alexander",
+    lastName: "Lewis",
+    email: "alexanderlewis@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "16",
+    firstName: "Charlotte",
+    lastName: "King",
+    email: "charlotteking@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "17",
+    firstName: "William",
+    lastName: "Scott",
+    email: "williamscott@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "18",
+    firstName: "Grace",
+    lastName: "Baker",
+    email: "gracebaker@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "19",
+    firstName: "Benjamin",
+    lastName: "Wright",
+    email: "benjaminwright@example.com",
+    image: "https://i.pravatar.cc/150"
+  },
+  {
+    id: "20",
+    firstName: "Lily",
+    lastName: "Turner",
+    email: "lilyturner@example.com",
+    image: "https://i.pravatar.cc/150"
+  }
+];
